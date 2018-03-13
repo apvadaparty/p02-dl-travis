86cd479308589c2e2a116d3eaf160d8b48e45287
Eric Carlson <ecarlson156@gmail.com>
2018-03-12 23:30:42 -0400

    hot shit yo

--- p02_fashion_mnist_experiments.sh
+++ p02_fashion_mnist_experiments.sh
@@ -3,55 +3,55 @@ set -e
 set -u
 set -x
 
-# Question 1 Default settings
-python p02_fashion_mnist.py --dataset mnist --epochs 10 --name q1_default
-python p02_fashion_mnist.py --dataset fashion_mnist --epochs 10 --name q1_default  --data_dir ../data/q1
-
-# Question 2
-python p02_fashion_mnist.py --dataset mnist --epochs 20 --name q1_20_epochs --data_dir ../data/q2
-python p02_fashion_mnist.py --dataset fashion_mnist --epochs 20 --name q1_20_epochs --data_dir ../data/q2
-
-
-# Question 3
-python p02_fashion_mnist.py --dataset fashion_mnist --lr 0.1 --name q3_0.1 --data_dir ../data/q3
-python p02_fashion_mnist.py --dataset fashion_mnist --lr 0.001 --name q_0.001 --data_dir ../data/q3
-
-# Question 4
-python p02_fashion_mnist.py --dataset fashion_mnist --optimizer adam --name q4_adam --data_dir ../data/q4
-python p02_fashion_mnist.py --dataset fashion_mnist --optimizer rmsprop --name q4_rmsprop --data_dir ../data/q4
-
-#Question 5
-python p02_fashion_mnist.py --dataset fashion_mnist --name q5_dr0  --dropout-rate 0 --data_dir ../data/q5
-python p02_fashion_mnist.py --dataset fashion_mnist --name q5_dr25 --dropout-rate 0.25  --data_dir ../data/q5
-python p02_fashion_mnist.py --dataset fashion_mnist --name q5_dr90 --dropout-rate 0.90 --data_dir ../data/q5
-python p02_fashion_mnist.py --dataset fashion_mnist --name q5_dr100 --dropout-rate 1 --data_dir ../data/q5
-
-#Question 6
-python p02_fashion_mnist.py --dataset fashion_mnist --name q6_bs_0.125 --batch-size 32 --data_dir ../data/q6
-python p02_fashion_mnist.py --dataset fashion_mnist --name q6_bs_8 --batch-size 2048 --data_dir ../data/q6
-
-#Question 7
-python p02_fashion_mnist.py --dataset fashion_mnist --name q7_P2Q7HalfChannelsNet --model P2Q7HalfChannelsNet --data_dir ../data/q7
-python p02_fashion_mnist.py --dataset fashion_mnist --name q7_P2Q7DoubleChannelsNet --model P2Q7DoubleChannelsNet --data_dir ../data/q7
-
-#Question 8
-python p02_fashion_mnist.py --dataset fashion_mnist --name q8_P2Q8BatchNormNet --model P2Q8BatchNormNet --data_dir ../data/q8
-
-#Question 9
-python p02_fashion_mnist.py --dataset fashion_mnist --name q9_P2Q9DropoutNet --model P2Q9DropoutNet --data_dir ../data/q9
-
-#Question 10
-python p02_fashion_mnist.py --dataset fashion_mnist --name q10_P2Q10DropoutBatchnormNet --model P2Q10DropoutBatchnormNet --data_dir ../data/q10
-
-#Question 11
-python p02_fashion_mnist.py --dataset fashion_mnist --name q11_extraconv --data_dir ../data/fashion_mnist --model P2Q11ExtraConvNet
-
-#Question 12
-python p02_fashion_mnist.py --dataset fashion_mnist --name q12_rmlayer --data_dir ../data/fashion_mnist --model P2Q12RemoveLayerNet
+# # Question 1 Default settings
+# python p02_fashion_mnist.py --dataset mnist --epochs 10 --name q1_default
+# python p02_fashion_mnist.py --dataset fashion_mnist --epochs 10 --name q1_default  --data_dir ../data/q1
+#
+# # Question 2
+# python p02_fashion_mnist.py --dataset mnist --epochs 20 --name q1_20_epochs --data_dir ../data/q2
+# python p02_fashion_mnist.py --dataset fashion_mnist --epochs 20 --name q1_20_epochs --data_dir ../data/q2
+#
+#
+# # Question 3
+# python p02_fashion_mnist.py --dataset fashion_mnist --lr 0.1 --name q3_0.1 --data_dir ../data/q3
+# python p02_fashion_mnist.py --dataset fashion_mnist --lr 0.001 --name q_0.001 --data_dir ../data/q3
+#
+# # Question 4
+# python p02_fashion_mnist.py --dataset fashion_mnist --optimizer adam --name q4_adam --data_dir ../data/q4
+# python p02_fashion_mnist.py --dataset fashion_mnist --optimizer rmsprop --name q4_rmsprop --data_dir ../data/q4
+#
+# #Question 5
+# python p02_fashion_mnist.py --dataset fashion_mnist --name q5_dr0  --dropout-rate 0 --data_dir ../data/q5
+# python p02_fashion_mnist.py --dataset fashion_mnist --name q5_dr25 --dropout-rate 0.25  --data_dir ../data/q5
+# python p02_fashion_mnist.py --dataset fashion_mnist --name q5_dr90 --dropout-rate 0.90 --data_dir ../data/q5
+# python p02_fashion_mnist.py --dataset fashion_mnist --name q5_dr100 --dropout-rate 1 --data_dir ../data/q5
+#
+# #Question 6
+# python p02_fashion_mnist.py --dataset fashion_mnist --name q6_bs_0.125 --batch-size 32 --data_dir ../data/q6
+# python p02_fashion_mnist.py --dataset fashion_mnist --name q6_bs_8 --batch-size 2048 --data_dir ../data/q6
+#
+# #Question 7
+# python p02_fashion_mnist.py --dataset fashion_mnist --name q7_P2Q7HalfChannelsNet --model P2Q7HalfChannelsNet --data_dir ../data/q7
+# python p02_fashion_mnist.py --dataset fashion_mnist --name q7_P2Q7DoubleChannelsNet --model P2Q7DoubleChannelsNet --data_dir ../data/q7
+#
+# #Question 8
+# python p02_fashion_mnist.py --dataset fashion_mnist --name q8_P2Q8BatchNormNet --model P2Q8BatchNormNet --data_dir ../data/q8
+#
+# #Question 9
+# python p02_fashion_mnist.py --dataset fashion_mnist --name q9_P2Q9DropoutNet --model P2Q9DropoutNet --data_dir ../data/q9
+#
+# #Question 10
+# python p02_fashion_mnist.py --dataset fashion_mnist --name q10_P2Q10DropoutBatchnormNet --model P2Q10DropoutBatchnormNet --data_dir ../data/q10
+#
+# #Question 11
+# python p02_fashion_mnist.py --dataset fashion_mnist --name q11_extraconv --data_dir ../data/fashion_mnist --model P2Q11ExtraConvNet
+#
+# #Question 12
+# python p02_fashion_mnist.py --dataset fashion_mnist --name q12_rmlayer --data_dir ../data/fashion_mnist --model P2Q12RemoveLayerNet
 
 #Question 13
-#python p02_fashion_mnist.py --dataset fashion_mnist --name q13_ultimateNetgm --data_dir ../data/fashion_mnist --model P2Q13UltimateNet
+python p02_fashion_mnist.py --dataset fashion_mnist --name q13 --data_dir ../data/fashion_mnist --model P2Q13UltimateNet --batch-size 32 --epochs 10 --optimizer adam
 
 
-#Question 15
-#python p02_fashion_mnist.py --dataset mnist --name q15_lr20x_mnist --data_dir ../data --model P2Q13UltimateNet --lr 0.0005
+# #Question 15
+# python p02_fashion_mnist.py --dataset mnist --name q15_lr20x_mnist --data_dir ../data --model P2Q13UltimateNet --lr 0.0005
